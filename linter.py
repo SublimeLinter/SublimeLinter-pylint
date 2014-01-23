e222883ec151f8680a101dc81185fff367f47b63
NotSqrt <notsqrt@gmail.com>
2014-01-23 12:19:22 +0100

    Fix regex to match msg-template

--- linter.py
+++ linter.py
@@ -30,7 +30,7 @@ class Pylint(PythonLinter):
     version_re = r'^pylint (?P<version>\d+\.\d+\.\d+),'
     version_requirement = '>=1.0'
     regex = (
-        r'^(?P<line>\d+):(?P<col>\d+): '
+        r'^(?P<line>\d+):(?P<col>\d+):'
         r'(?:(?P<error>[RFE])|(?P<warning>[CIW]))\d+:'
         r'(?P<message>.*)'
     )
